d1b9783781c04ebac7aa8a53149864deefdc2222
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-06-02 17:51:11 +0530

    Revmap the ts and index

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const text = document.querySelector("p");
 
-addEventListener('mousemove', (e)=>{
+window.addEventListener('mousemove', (e)=>{
     text.style.left = (e.clientX + 15) + 'px';
     text.style.top = (e.clientY + 15) + 'px';
+});
 
-})
+
